bb3b49f9eafea7481c4d8ca6487325bbcbce68bc
lizhizhou <lzz@meteroi.com>
2015-10-01 20:40:05 -0700

    add useradd

--- firstboot.sh
+++ firstboot.sh
@@ -5,4 +5,5 @@ sudo cp ../sources.list etc/apt
 sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LANG=en_US.UTF-8 chroot . apt-get update
 sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LANG=en_US.UTF-8 chroot . dpkg --set-selections < dpkg-get-selections
 sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LANG=en_US.UTF-8 chroot . apt-get -y -u dselect-upgrade
+sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LANG=en_US.UTF-8 chroot . useradd -g root -d /home/openfpgaduino -s /bin/bash -m openfpgaduino
 sudo rm -rf dev

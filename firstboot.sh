b009ff38c8a414f00d436ecc0eecd0f02e48ba43
lizhizhou <lzz@meteroi.com>
2015-10-10 09:11:31 -0700

    seperate second stage

--- firstboot.sh
+++ firstboot.sh
@@ -1,7 +1,6 @@
-/debootstrap/debootstrap --second-stage
-apt-get update
-dpkg --set-selections < dpkg-get-selections
-apt-get -y -u dselect-upgrade
 useradd -g root -d /home/openfpgaduino -s /bin/bash -m openfpgaduino
 adduser openfpgaduino sudo
 echo '127.0.0.1    openfpgaduino' >> /etc/hosts
+apt-get update
+dpkg --set-selections < dpkg-get-selections
+apt-get -y -u dselect-upgrade
--- secondstage.sh
+++ secondstage.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-cd fs;
-sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot . ./firstboot.sh 
+ed#!/bin/sh
+cd fs; sudo chroot . /debootstrap/debootstrap --second-stage
+sudo chroot . ./firstboot.sh 
 

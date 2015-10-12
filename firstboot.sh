910b980d0c935dac09fc2fbc7d833b2e306b3d58
lizhizhou <lzz@meteroi.com>
2015-10-12 06:31:45 -0700

    make pingable in user mode

--- firstboot.sh
+++ firstboot.sh
@@ -10,6 +10,7 @@ lab123
 PSWD
 echo '127.0.0.1    openfpgaduino' >> /etc/hosts
 echo 'nameserver   8.8.8.8' >> /etc/resolv.conf
+chmod u+s /bin/ping
 apt-get update
 dpkg --set-selections < dpkg-get-selections
 apt-get -y -u dselect-upgrade
